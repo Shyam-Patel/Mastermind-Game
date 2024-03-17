af33858857ca27e095bc86792cbf103d9d8b1294
Shyam Patel <shyam.p95@gmail.com>
2024-03-17 14:48:57 -0400

    Initial commit (created solution)

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Mastermind_Game
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
