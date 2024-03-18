e93817e64310c56f3b7f322877a0c7c45b30a31d
Shyam Patel <shyam.p95@gmail.com>
2024-03-17 20:16:48 -0400

    Add unit test cases for the CodeAnalyzer and Mastermind objects

--- MastermindGame.Tests/CodeAnalyzerTests.cs
+++ MastermindGame.Tests/CodeAnalyzerTests.cs
@@ -8,5 +8,39 @@ namespace MastermindGame.Tests
 {
     public class CodeAnalyzerTests
     {
+        [Fact]
+        public void GenerateCode_GeneratesCorrectLength()
+        {
+            int expectedLength = 4;
+
+            var code = CodeAnalyzer.GenerateCode(expectedLength);
+            Assert.Equal(expectedLength, code.Length);
+        }
+
+        [Fact]
+        public void GenerateCode_GeneratesOnlyDigits()
+        {
+            var code = CodeAnalyzer.GenerateCode(4);
+            Assert.True(int.TryParse(code, out _));
+        }
+
+        [Fact]
+        public void ParseGuess_ReturnsTrueForCorrectGuess()
+        {
+            var code = "4325";
+            Assert.True(CodeAnalyzer.ParseGuess(code, "4325"));
+        }
+
+        [Fact]
+        public void ParseGuess_ReturnsFalseForIncorrectGuess()
+        {
+            using (StringWriter sw = new StringWriter())
+            {
+                Console.SetOut(sw);
+
+                var code = "4325";
+                Assert.False(CodeAnalyzer.ParseGuess(code, "2124"));
+            }
+        }
     }
 }
--- MastermindGame.Tests/MastermindTests.cs
+++ MastermindGame.Tests/MastermindTests.cs
@@ -3,9 +3,17 @@ namespace MastermindGame.Tests
     public class MastermindTests
     {
         [Fact]
-        public void Test1()
+        public void PrintRulesToConsole_ShouldPrint()
         {
+            using (StringWriter sw = new StringWriter())
+            {
+                Console.SetOut(sw); //captures output to the console
 
+                var masterMindGame = new Mastermind();
+                masterMindGame.PrintRulesToConsole();
+
+                Assert.True(!string.IsNullOrEmpty(sw.ToString()));
+            }
         }
     }
 }
